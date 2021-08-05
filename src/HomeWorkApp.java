a892c7a21296270243e575d2b0477f31fb2d954b
GattoFritto <88328247+GattoFritto@users.noreply.github.com>
2021-08-05 18:08:52 +0300

    ДЗ №2

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -1,53 +1,48 @@
 public class HomeWorkApp {
     public static void main(String[] args){
-        printThreeWords();
 
-        checkSumSign();
+        int a = 4;
+        int b = 8;
+        boolean betweenLimit = sumLimitMethod(a, b);
+        System.out.println(betweenLimit);
 
-        printColor();
+        int d = -7;
+        positiveNegative(d);
 
-        compareNumbers();
+        int e = -2;
+        boolean negNumber = negativeMethod(e);
+        System.out.println(negNumber);
+
+        int f = 3;
+        wordsPrints("hello!", 3);
     }
 
-    private static void compareNumbers() {
-        int a = 88;
-        int b = 66;
-        if (a >= b){
-            System.out.println("a >= b");
+    public static boolean sumLimitMethod(int a, int b) {
+        int c = a + b;
+        if (10 <= c && c <= 20) {
+            return true;
         }
-        if (a < b){
-            System.out.println("a < b");
-        }
+        return false;
     }
 
-    private static void printColor() {
-        int value = 8;
-        if (value  <= 0){
-            System.out.println("Красный");
+    public static void positiveNegative(int d) {
+        if (d >= 0) {
+            System.out.println("Положительное");
         }
-        if (value > 0 && value <= 100){
-            System.out.println("Желтый");
-        }
-        if (value > 100){
-            System.out.println("Зеленый");
+        else {
+            System.out.println("Отрицательное");
         }
     }
-
-    private static void checkSumSign() {
-        int a = -4;
-        int b = 7;
-        if (a + b > 0){
-            System.out.println("Сумма положительная");
+    public static boolean negativeMethod(int e) {
+        if (e < 0) {
+            return true;
         }
-        if (a + b < 0){
-            System.out.println("Сумма отрицательная");
-        }
+        return false;
     }
-
-    private static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+    private static void wordsPrints(String word, int f) {
+        for (int g =0; g < f; g++) {
+            System.out.println(word);
+        }
     }
 }
 // Комментарий
